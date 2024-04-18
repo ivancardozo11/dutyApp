a0c3179ce76ff00e610f1ecb7e146477d38dfee3
Ivan Cardozo <ivancardozo11@hotmail.com>
2024-04-18 10:37:16 +0200

    Improve code documentation, create apiClient file, implement cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,13 @@
 import express, { Request, Response, NextFunction, Application } from 'express';
 import dutyRoutes from './routes/dutyRoutes';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: Application = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 interface HttpException extends Error {
     status?: number;
--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3000';
 
 const handleResponse = async (response: Response) => {
   if (!response.ok) {
